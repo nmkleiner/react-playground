59cdc1fab71468d84840f4a898cad74c6a20da1f
Noam Kleiner <noam.k@compie.co.il>
2020-10-05 10:31:10 +0300

    Refactoring: Logo, clean code

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -3,8 +3,4 @@ import logo from "../../logo.svg";
 import "./style.scss";
 
 
-export const Logo = () => {
-    return (
-        <img src={logo} className="App-logo" alt="logo"/>
-    );
-};
+export const Logo = () => <img src={logo} className="AppLogo" alt="logo"/>;
